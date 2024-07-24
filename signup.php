11dce84b00ff87bb6f78e303a2943bddfc173fca
parmar-viral <parmarviral8324@gmail.com>
2024-07-24 13:41:40 +0530

    update

--- signup.php
+++ signup.php
@@ -15,6 +15,7 @@ include 'admin/controller/user_controller.php';
 </head>
 
 <body>
+<?php include 'menu.php';?>
     <div class="container-fluid ">
 
         <div class="row  d-flex  justify-content-center  mt-3">
